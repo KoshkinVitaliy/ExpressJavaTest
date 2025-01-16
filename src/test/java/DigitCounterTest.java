da841fdc77abe390cbb6db94d7765d6c0eee6247
KoshkinVitaliy <kosh98vitaliy@outlook.com>
2025-01-16 16:50:06 +0300

    Correct version of tests

--- src/test/java/DigitCounterTest.java
+++ src/test/java/DigitCounterTest.java
@@ -7,25 +7,25 @@ public class DigitCounterTest {
 
     @Test
     public void testCountDigitsWithNoDuplicates() {
-        assertEquals(1, digitCounter.countDigits(new int[]{3, 7, 5}));
-        assertEquals(1, digitCounter.countDigits(new int[]{1, 2, 3, 4, 5}));
+        assertEquals(3, digitCounter.countDigits(new int[]{3, 7, 5}));
+        assertEquals(5, digitCounter.countDigits(new int[]{1, 2, 3, 4, 5}));
     }
 
     @Test
     public void testCountDigitsWithDuplicates() {
         assertEquals(2, digitCounter.countDigits(new int[]{0, 4, 6, 6, 6, 8, 8, 7}));
-        assertEquals(1, digitCounter.countDigits(new int[]{1, 1, 2, 2, 3, 3}));
+        assertEquals(3, digitCounter.countDigits(new int[]{1, 1, 2, 2, 3, 3}));
     }
 
     @Test
     public void testCountDigitsWithMixedDuplicates() {
-        assertEquals(1, digitCounter.countDigits(new int[]{1, 2, 2, 3, 3, 3}));
-        assertEquals(3, digitCounter.countDigits(new int[]{0, 0, 1, 1, 1, 2, 2}));
+        assertEquals(2, digitCounter.countDigits(new int[]{1, 2, 2, 3, 3, 3}));
+        assertEquals(2, digitCounter.countDigits(new int[]{0, 0, 1, 1, 1, 2, 2}));
     }
 
     @Test
     public void testCountDigitsWithAllSame() {
-        assertEquals(6, digitCounter.countDigits(new int[]{6, 6, 6, 6}));
-        assertEquals(9, digitCounter.countDigits(new int[]{9, 9, 9, 9, 9}));
+        assertEquals(4, digitCounter.countDigits(new int[]{6, 6, 6, 6}));
+        assertEquals(5, digitCounter.countDigits(new int[]{9, 9, 9, 9, 9}));
     }
 }
